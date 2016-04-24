dd9713e75a1e727c1379bf48eb4997135bcc1a5a
José Luis Olivares <otrofama@gmail.com>
2016-04-23 23:14:29 -0500

    parser

--- token.h
+++ token.h
@@ -27,30 +27,30 @@
 #define RETURN		18
 #define DEFAULT		19
 #define BREAK		20
-#define DPUN		21 		
-#define PCOM		22 		
-#define COMA		23 		
-#define PAR_AB		24
-#define PAR_CI		25 		
-#define LLA_AB		26 		
-#define LLA_CI		27 		
-#define MAS			28 		
-#define MENOS		29 		
-#define POR			30 		
-#define DIV			31 		
-#define MOD			32 		
-#define PIC_CI		33		
-#define PIC_AB		34 		
-#define MAY_IG		35 		
-#define MEN_IG		36 		
-#define DOB_IG		37 		
-#define DIF			38 		
-#define _AND		39 		
-#define _OR			40 		
-#define _NOT		41 		
-#define DMAS		43 		
-#define DMENOS		44 		
-#define IGUAL		46 		
+#define DPUN		21 		//:
+#define PCOM		22 		//;
+#define COMA		23 		//,
+#define PAR_AB		24		//(
+#define PAR_CI		25 		//)
+#define LLA_AB		26 		//{
+#define LLA_CI		27 		//}
+#define MAS			28 		//+
+#define MENOS		29 		//-
+#define POR			30 		//*
+#define DIV			31 		// /
+#define MOD			32 		//%
+#define PIC_CI		33		//>
+#define PIC_AB		34 		//<
+#define MAY_IG		35 		//>=
+#define MEN_IG		36 		//<=
+#define DOB_IG		37 		//==
+#define DIF			38 		//<>
+#define _AND		39 		//&&
+#define _OR			40 		//||
+#define _NOT		41 		// !
+#define DMAS		43 		//++
+#define DMENOS		44 		//--
+#define IGUAL		46 		//=
 
 
 typedef union YYSTYPE YYSTYPE;
